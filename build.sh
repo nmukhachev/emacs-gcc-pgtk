520e1da2fd53275a33cf177c5c6c68e89bb36de6
nmukhachev <62498555+nmukhachev@users.noreply.github.com>
2023-03-12 21:45:21 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-docker build -t konstare/emacs-gcc-pgtk:latest .
-id=$(docker create konstare/emacs-gcc-pgtk)
+docker build -t nmukhachev/emacs-gcc-pgtk:latest .
+id=$(docker create nmukhachev/emacs-gcc-pgtk)
 docker cp $id:/opt/deploy .
